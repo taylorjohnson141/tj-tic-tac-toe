9d5fc8552d5e62d82c02cd980751c293330ded94
Taylor Johnson <taylorjohnson141@gmail.com>
2020-07-30 16:46:50 -0600

    Add Game class with id icon and closed properties

---
+++ src/game.js
@@ -0,0 +1,51 @@
+class Game{
+  constructor() {
+    [
+      {
+        id: 1,
+        icon: '',
+        closed: false,
+      },
+      {
+        id: 2,
+        icon: '',
+        closed: false,
+      },
+      {
+        id: 3,
+        icon: '',
+        closed: false,
+      },
+      {
+        id: 4,
+        icon: '',
+        closed: false,
+      },
+      {
+        id: 5,
+        icon: '',
+        closed: false,
+      },
+      {
+        id: 6,
+        icon: '',
+        closed: false,
+      },
+      {
+        id: 7,
+        icon: '',
+        closed: false,
+      },
+      {
+        id: 8,
+        icon: '',
+        closed: false,
+      },
+      {
+      id: 9,
+      icon: '',
+      closed: false,
+    }
+    ]
+  }
+}
