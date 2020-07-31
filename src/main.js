c3b301ac46862a65da23b7e5d4920c394c1b5edc
Taylor Johnson <taylorjohnson141@gmail.com>
2020-07-31 11:06:03 -0600

    Add events for wrapper click event

--- src/main.js
+++ src/main.js
@@ -8,10 +8,16 @@ window.addEventListener('load', function () {
   game.playerTwo.retrieveWinsFromStorage()
 
 });
-
+wrapper.addEventListener('click',function(){
+  if(game.board[event.target.data.id].closed !==true){
+    console.log(event.target)
+    game.board[event.target.data.id].icon = game.currentPlayer.icon
+    game.board[event.target.data.id].closed = true
+  }
+})
 
 function initGame() {
-  var playerOne = new Player(1, '', 0);
-  var playerTwo = new Player(2, '', 0);
+  var playerOne = new Player(1, 'a', 0);
+  var playerTwo = new Player(2, 'b', 0);
   return new Game(playerOne, playerTwo);
 }
