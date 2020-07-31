f1e9753fccb3c83a0995ccf8163bc9b09b009908
Taylor Johnson <taylorjohnson141@gmail.com>
2020-07-30 19:01:23 -0600

    Add eventlistener to window

---
+++ src/main.js
@@ -0,0 +1,14 @@
+document.querySelector('wrapper');
+
+game;
+
+window.addEventListener('load', function () {
+  game = initGame();
+});
+
+
+function initGame() {
+  var playerOne = new Player(1, '', 0);
+  var playerTwo = new Player(2, '', 0);
+  return new Game(playerOne, playerTwo);
+}
