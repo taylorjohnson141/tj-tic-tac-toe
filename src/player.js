506d8cfefc8fc33c31c5f068274611d418c7534d
Taylor Johnson <taylorjohnson141@gmail.com>
2020-07-31 20:14:15 -0600

    Refactor add to wins and add wins from local storage

--- src/player.js
+++ src/player.js
@@ -4,15 +4,17 @@ this.id = id,
 this.icon = icon,
 this.wins = wins
 }
-addWin(win) {
-  this.wins += win;
+addWin() {
+  this.wins += 1;
 }
+
 saveWinsToStorage() {
   localStorage.setItem(id,JSON.stringify(this.wins));
 }
 retrieveWinsFromStorage(){
   if(localStorage.length !==0){
-  localStorage.JSON.parse(getItem(id,this.wins));
+  var winsInlocal = localStorage.JSON.parse(getItem(id,this.wins));
+  this.wins += parseInt(winsInlocal)
 }
 }
 
