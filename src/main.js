268f2a2539e1f30cd9d41b76a022286318472ce8
Taylor Johnson <taylorjohnson141@gmail.com>
2020-08-02 12:49:17 -0600

    Add clear buttons

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 var gameWrapper = document.querySelector('.wrapper');
 var gameBoard =  gameWrapper.querySelectorAll('.game-board');
 var game;
-var overlay = document.querySelector('.overlay')
+var overlay = document.querySelector('.overlay');
 var playerOneWins = document.querySelector('.player-one');
 var playerTwoWins = document.querySelector('.player-two');
-
+var clearWinsButton = document.querySelector('.clear-wins');
+var clearBoardButton = document.querySelector('.clear-board');
 window.addEventListener('load', function () {
   game = initGame();
   if (localStorage.key('currentGame') === undefined || localStorage.key('currentGame') === null || game.retrieveGameFromStorage() === undefined ) {
-    playerOneWins.innerText += game.playerOne.retrieveWinsFromStorage();
-    playerTwoWins.innerText += game.playerTwo.retrieveWinsFromStorage();
+    addWinstoPlayers()
   }
   // else {
   //   game = initGame();
@@ -19,6 +19,12 @@ window.addEventListener('load', function () {
   //   playerTwoWins.innerText += game.playerTwo.retrieveWinsFromStorage();
   // }
 });
+clearWinsButton.addEventListener('click', clearLocalStorage)
+clearBoardButton.addEventListener('click',function(){
+  clearHTML()
+  game = initGame();
+
+})
 
 gameWrapper.addEventListener('click', function () {
   var location = parseInt(event.target.dataset['id']) - 1;
@@ -44,6 +50,7 @@ gameWrapper.addEventListener('click', function () {
       // addOverlay()
       game.currentPlayer.addWin();
       game.currentPlayer.saveWinsToStorage()
+      addWinstoPlayers()
       // turnOffClicks(gameBoard,result);
       clearHTMLafterTwoSecond()
       game.removeGameFromLocalStorage()
@@ -70,6 +77,7 @@ gameWrapper.addEventListener('click', function () {
 function initGame() {
   var playerOne = new Player(1, 'a', 0);
   var playerTwo = new Player(2, 'b', 0);
+
   return new Game(playerOne, playerTwo);
 }
 function clearHTML() {
@@ -110,4 +118,23 @@ function addOverlay(){
 function removeOverlay(){
   overlay.classList.add('hidden')
 
+}
+function addWinstoPlayers(){
+  game.playerOne.wins = game.playerOne.retrieveWinsFromStorage()
+  game.playerTwo.wins = game.playerTwo.retrieveWinsFromStorage()
+  if(game.playerOne.wins === null || game.playerOne.wins === undefined  || isNaN(game.playerTwo.wins)){
+    game.playerOne.wins = 0
+  }
+  if(game.playerTwo.wins === null || game.playerTwo.wins === undefined || isNaN(game.playerTwo.wins) ){
+    game.playerTwo.wins = 0
+  }
+
+  console.log('should be number',typeof playerTwoWinsNumber)
+  playerOneWins.innerText = `Player One Wins: ${game.playerOne.wins }`;
+  playerTwoWins.innerText =  `Player Two Wins: ${game.playerTwo.wins }`;
+}
+
+function clearLocalStorage(){
+  localStorage.clear();
+  addWinstoPlayers();
 }
